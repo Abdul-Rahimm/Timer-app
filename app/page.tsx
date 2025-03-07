d568ec34b06f61b58eb130a130c17d8cd18ab782
Abdul-Rahimm <k213951@nu.edu.pk>
2025-03-07 14:22:36 +0500

    rahim branch init

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
-'use client'
+"use client";
 import { useState, useEffect } from "react";
+
+// Createed rahim branch
 
 export default function Timer() {
   const [seconds, setSeconds] = useState(0);
