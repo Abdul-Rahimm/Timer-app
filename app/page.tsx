5cf4208ddcb61b1ffef85d1719353aacc437157b
Abdul-Rahimm <k213951@nu.edu.pk>
2025-03-07 15:59:59 +0500

    sign In logic is done.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,25 @@
 "use client";
 import HuzzTimer from "@/components/HuzzTimer";
+import { useSession, signIn } from "next-auth/react";
 // Createed rahim branch
 
-export default function Timer() {
+export default function Home() {
+  const { data: session } = useSession();
+
+  if (!session) {
+    return (
+      <div className="bg-blue-900 w-screen h-screen flex flex-col items-center justify-center">
+        <div className="bg-white text-black p-4 rounded-lg font-semibold text-xl my-4">
+          <button onClick={() => signIn("google")}>Login with Google</button>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="bg-blue-900 w-screen h-screen flex flex-col items-center justify-center">
       <div className="bg-white text-black p-4 rounded-lg font-semibold text-xl my-4">
-        Login with Google
+        Hello, {session.user?.name}
       </div>
       <HuzzTimer />
     </div>
---
+++ lib/auth.ts
@@ -0,0 +1,15 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+// This code defines your NextAuth configuration
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
